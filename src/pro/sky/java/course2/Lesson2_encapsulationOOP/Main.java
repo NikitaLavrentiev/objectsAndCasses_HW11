8d64f3fb6c8f57679777921a747746c4318ca510
Никита <lavrentivev.nikita@gmail.com>
2023-01-14 00:06:57 +0300

    отредактировал сеттеры, создал класс Key и добавил его в конструкторы

--- src/pro/sky/java/course2/Lesson2_encapsulationOOP/Main.java
+++ src/pro/sky/java/course2/Lesson2_encapsulationOOP/Main.java
@@ -13,12 +13,12 @@ public class Main {
         //Kia Sportage 4-го поколения, 2018 год выпуска, сборка в Южной Корее, цвет кузова — красный, объем двигателя — 2,4 л.
         //Hyundai Avante, сборка в Южной Корее, цвет кузова — оранжевый, объем двигателя — 1,6 л, год выпуска — 2016 год.
         //Напишите программу, которая будет выводить в консоль информацию о каждом автомобиле и все указанные выше характеристики.
-        Car ladaGranta = new Car("Lada","Granta", 2015, "Россия" , "желтый",  1.7);
-        Car audiA8 = new Car("Audi", "A8 50 L TDI quattro", 2020, "Германия", "чёрный", 3.0);
-        Car bMW = new Car("BMW", "Z8", 2021, "Германия", "чёрный", 3.0);
-        Car kia = new Car("Kia", "Sportage 4-го поколения", 2018, "Южная корея", "красный", 2.4);
-        Car hyundaiAvante = new Car("Hyundai", "Avante", 2016, "Южная корея", "орнжевый", 1.6);
-        Car empty = new Car("","",-1000,"","",0);
+        Car ladaGranta = new Car("Lada","Granta", 2015, "Россия" , "желтый",  1.7, "auto","",0, 5,true);
+        Car audiA8 = new Car("Audi", "A8 50 L TDI quattro", 2020, "Германия", "чёрный", 3.0, "auto","",0, 5,true);
+        Car bMW = new Car("BMW", "Z8", 2021, "Германия", "чёрный", 3.0,"auto","",0, 5,true);
+        Car kia = new Car("Kia", "Sportage 4-го поколения", 2018, "Южная корея", "красный", 2.4,"auto","",0, 5,true);
+        Car hyundaiAvante = new Car("Hyundai", "Avante", 2016, "Южная корея", "орнжевый", 1.6,"auto","",0, 5,true);
+        Car empty = new Car("","",-1000,"","",0,"auto","",0, 5,true);
 
         System.out.println(ladaGranta.toString());
         System.out.println(empty);
--- src/pro/sky/java/course2/Lesson2_encapsulationOOP/transport/Car.java
+++ src/pro/sky/java/course2/Lesson2_encapsulationOOP/transport/Car.java
@@ -1,6 +1,8 @@
 package pro.sky.java.course2.Lesson2_encapsulationOOP.transport;
 
 import java.time.LocalDate;
+import java.util.Objects;
+
 public class Car {
 
     /*Создайте пакет transport и перенесите туда класс Car.
@@ -26,6 +28,19 @@ public class Car {
     private static final Integer DEFAULT_VALUE_OF_SEATS = 4;
     private static final double DEFAULT_ENGINE_VALUE = 1.5;
     private static final String DEFAULT_COLOUR = "white";
+    private Key key;
+
+    public Key getKye() {
+        return key;
+    }
+
+    public void setKey(Key key) {
+        if (key == null) {
+            this.key = new Key();
+        } else {
+            this.key = key;
+        }
+    }
 
     public Car(String brand, String model, int year, String country, String color, double engineVolume, String transmission, String bodyType, int registrationNumber, int numberOfSeats, boolean isSummerTyres) {
         if (brand.isBlank() || brand == null) {
@@ -33,7 +48,7 @@ public class Car {
         } else {
             this.brand = brand;
         }
-        if (model.isBlank() || model== null) {
+        if (model.isBlank() || model == null) {
             this.model = DEFAULT_VALUE;
         } else {
             this.model = model;
@@ -53,69 +68,80 @@ public class Car {
         } else {
             this.year = 2000;
         }
-        if (country.isBlank() || country== null) {
+        if (country.isBlank() || country == null) {
             this.country = DEFAULT_VALUE;
         } else {
             this.country = country;
         }
-        if (transmission.isBlank() || transmission== null) {
+        if (transmission.isBlank() || transmission == null) {
             this.transmission = "auto";
         } else {
-            this.transmission = transmission;}
+            this.transmission = transmission;
+        }
         if (bodyType.equals("Jeep") ||
                 bodyType.equals("Hatchback") || bodyType.equals("Sedan") || bodyType.equals("Coupe") ||
-            bodyType.equals("Wagon") || bodyType.equals("Van") || bodyType.equals("MUV/SUV") ||
+                bodyType.equals("Wagon") || bodyType.equals("Van") || bodyType.equals("MUV/SUV") ||
                 bodyType.equals("Convertible")) {
-            this.bodyType= bodyType;
-        } else if (bodyType.isBlank() || bodyType== null){
+            this.bodyType = bodyType;
+        } else if (bodyType.isBlank() || bodyType == null) {
             this.bodyType = DEFAULT_VALUE;
         } else {
             this.bodyType = DEFAULT_VALUE;
         }
-        if (registrationNumber>=0) {
+        if (registrationNumber >= 0) {
             this.registrationNumber = registrationNumber;
         } else {
             this.registrationNumber = countOfCars;
         }
-        if (numberOfSeats<=0) {
+        if (numberOfSeats <= 0) {
             this.numberOfSeats = DEFAULT_VALUE_OF_SEATS;
         } else {
             this.numberOfSeats = numberOfSeats;
         }
         this.isSummerTyres = isSummerTyres;
+        this.key = Objects.requireNonNullElseGet(key, Key::new);
         countOfCars++;
     }
 
+
+    public Car(String brand, int year, String country, String transmission, boolean isSummerTyres) {
+        this(brand, null, year, country, null, 0, transmission, null, 0, 0, true);
+    }
+
     public String getBrand() {
-            return brand;
+        return brand;
     }
 
     public String getModel() {
-            return model;
+        return model;
     }
 
     public double getEngineVolume() {
-            return engineVolume;
+        return engineVolume;
     }
 
     public String getColor() {
-            return color;
+        return color;
     }
 
     public int getYear() {
-            return year;
+        return year;
     }
 
     public String getCountry() {
-            return country;
+        return country;
     }
-// для сеттеров ниже отредактировать параметры
-public String getTransmission() {
-    return transmission;
-}
+
+    public String getTransmission() {
+        return transmission;
+    }
 
     public void setTransmission(String transmission) {
-        this.transmission = transmission;
+        if (transmission.isBlank() || transmission == null) {
+            this.transmission = "auto";
+        } else {
+            this.transmission = transmission;
+        }
     }
 
     public int getRegistrationNumber() {
@@ -123,15 +149,15 @@ public String getTransmission() {
     }
 
     public void setRegistrationNumber(int registrationNumber) {
-        this.registrationNumber = registrationNumber;
+        if (registrationNumber >= 0) {
+            this.registrationNumber = registrationNumber;
+        } else {
+            this.registrationNumber = countOfCars;
+        }
     }
 
     public static int getCountOfCars() {
         return countOfCars;
-    }
-
-    public static void setCountOfCars(int countOfCars) {
-        Car.countOfCars = countOfCars;
     }
 
     public int getNumberOfSeats() {
@@ -146,26 +172,57 @@ public String getTransmission() {
         return isSummerTyres;
     }
 
-    public void setSummerTyres(boolean summerTyres) {
-        isSummerTyres = summerTyres;
-    }
-
     public void setEngineVolume(double engineVolume) {
-        this.engineVolume = engineVolume;
+        if (engineVolume >= DEFAULT_ENGINE_VALUE) {
+            this.engineVolume = engineVolume;
+        } else {
+            this.engineVolume = DEFAULT_ENGINE_VALUE;
+        }
     }
 
     public void setColor(String color) {
-        this.color = color;
+        if (color.isBlank() || color == null) {
+            this.color = DEFAULT_COLOUR;
+        } else {
+            this.color = color;
+        }
     }
+
     public int getMonth() {
         return LocalDate.now().getMonthValue();
     }
+
     public void changeTiresForSeason() {
-        isSummerTyres = getMonth() >= 3 || getMonth() < 12;
+        isSummerTyres = getMonth() >= 4 && getMonth() < 12;
     }
+
     @Override
     public String toString() {
         return getBrand() + " " + getModel() + ", " + getYear() + " год выпуска, сборка в " + getCountry() + ", цвет " + getColor() + ", объём двигателя " + getEngineVolume() + " л.";
     }
 
+    public class Key {
+        /*В классе Car создайте вложенный класс «Ключ» (Key) с параметрами:
+«Удаленный запуск двигателя»,
+«Бесключевой доступ».*/
+        private final boolean REMOTE_ENGINE_START;
+        private final boolean KEYLESS_ACCESS;
+
+        public Key(boolean doesItHaveRemoteEngine, boolean doesItHaveKeylessAccess) {
+            this.REMOTE_ENGINE_START = doesItHaveRemoteEngine;
+            this.KEYLESS_ACCESS = doesItHaveKeylessAccess;
+        }
+
+        public Key() {
+            this(false, false);
+        }
+
+        public boolean isREMOTE_ENGINE_START() {
+            return REMOTE_ENGINE_START;
+        }
+
+        public boolean isKEYLESS_ACCESS() {
+            return KEYLESS_ACCESS;
+        }
+    }
 }

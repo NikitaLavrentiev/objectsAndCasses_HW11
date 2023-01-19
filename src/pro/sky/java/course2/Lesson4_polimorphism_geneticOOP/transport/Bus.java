117ff9a09380236939649977bc5b148d0baf86fc
Никита <lavrentivev.nikita@gmail.com>
2023-01-19 05:22:24 +0300

    провёл подготовку для выполнения задания

---
+++ src/pro/sky/java/course2/Lesson4_polimorphism_geneticOOP/transport/Bus.java
@@ -0,0 +1,7 @@
+package pro.sky.java.course2.Lesson4_polimorphism_geneticOOP.transport;
+
+public class Bus extends Transport {
+    public Bus(String brand, String model, int year, String country, String color, int speed) {
+        super(brand, model, year, country, color, speed);
+    }
+}
